829c4406aaad594cb24c0ce38a2ad90885159680
Jonathan Hester <hester.jonathan@gmail.com>
2012-03-13 13:55:54 -0700

    getting rid of projects folder

--- src/com/jonathanhester/friendwatcher/Util.java
+++ src/com/jonathanhester/friendwatcher/Util.java
@@ -53,8 +53,8 @@ public class Util {
 	private static final String ENVIRONMENT_PROD = "prod";
 	private static final String ENVIRONMENT_LOCAL = "local1";
 	
-	private static final String ENVIRONMENT = ENVIRONMENT_PROD;
-	//private static final String ENVIRONMENT = ENVIRONMENT_LOCAL;
+	//private static final String ENVIRONMENT = ENVIRONMENT_PROD;
+	private static final String ENVIRONMENT = ENVIRONMENT_LOCAL;
 
 	// Shared constants
 
